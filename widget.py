415bb3c1dc3e6b2583bc87ffffd85367a86833c1
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-12-06 10:42:42 -0500

    maybe it'll work

--- widget.py
+++ widget.py
@@ -1,52 +1,68 @@
 #!/usr/bin/env python3
 
 from gi.repository import Gtk, Gio, WebKit
-import os
 
-template_head='<a href="#{text}"><th>{text}: {num}</th></a>'
-template_body='<a href="{file}#{line}"><td>{line}</td><td>{text}</td></tr></a>'
-
-class TodoPanel(WebKit.WebView):
-  def __init__(self, window, matches, key='TODO'):
-    WebKit.WebView.__init__(self)
+class TodoPanel(Gtk.Notebook):
+  def __init__(self, window, matches):
+    Gtk.Notebook.__init__(self)
     self.window  = window
     self.matches = matches
-    self.page    = key
+    self.pages = []
+
+    #TODO - put pages in notebook
+    for match in matches.keys():
+      page = Page(match, matches[match])
+      self.pages.append(page)
+      self.append_page(page, page.get_name())
 
   def update(self):
-    head = '<table width=100%><tr>'
-    body = ''
-    for tag in self.matches.keys():
-      count = 0
-      if tag == self.page:
-        body += '<table>'
-        for file in self.matches[tag].keys():
-          body += '<a href="?{path}"><tr><td colspan="2">{name}</td></tr></a>'
-          path=file.partition('://')[2]
-          body = body.format(path=path, name=os.path.basename(path))
-          for line, comment in self.matches[tag][file]:
-            body += template_body.format(file=file, line=line, text=comment)
-            count += 1
-        body += '</table>'
-      else:
-        for file in self.matches[tag].keys():
-          count += len(self.matches[tag][file])
-      head += template_head.format(text=tag, num=count)
-    head += '</tr></table>'
-    self.load_string('<html><body>'+head+body+'</html></body>',
-                     'text/html', 'utf-8', 'file://')
+    self.hide()
+    for page in self.pages:
+      page.update()
+    self.show()
 
-  def on_navigation_request(self, page, frame, request):
-    if request.get_uri().startswith('#'):
+class Page(Gtk.ScrolledWindow):
+  def __init__(self, name="", match={}):
+    Gtk.ScrolledWindow.__init__(self)
+    self.name    = Gtk.Label(name)
+    self.match   = match
+    self.html = '<a href="{0}#{1}"><tr><td>{1}</td><td>{2}</td></tr></a>'
+    self.file_label = Gtk.Label()
+    self.file_label.set_width_chars(-1)
+    self.file_label.set_ellipsize(True)
+    self.add(self.file_label)
+    self.update()
+
+  def update(self):
+    label_html = ''
+    for file_uri in sorted(self.match.keys()):
+      label_html += '<b>{}</b><table>'.format(self.name.rpartition('/')[2])
+      for line, text in sorted(elf.match[file_uri]):
+        label_html += self.html.format(file_uri, line, text)
+      label_html += '</table>'
+    self.file_label.set_lines(len(label_html.split('<tr>')))
+    self.file_label.set_markup(label_html)
+
+  def get_name(self):
+    return self.name
+
+  def set_name(self, name):
+    self.name = Gtk.Label(name)
+
+  def set_match(self, match):
+    self.match = match
+
+  def do_activate_link(self, uri):
+    if uri.startswith('#'):
       self.page = request.get_uri().partition('#')[2]
       self.update()
       return
-    elif request.get_uri().startswith('?'):
-      file_uri = 'file://'+request.get_uri().partition('?')[2]
+    elif uri.startswith('?'):
+      file_uri = 'file://'+uri.partition('?')[2]
       line     = 0
     else:
-      file_uri = request.get_uri().rpartition('#')[0]
-      line     = int(request.get_uri().rpartition('#')[2])-1
+      file_uri = uri.rpartition('#')[0]
+      line     = int(uri.rpartition('#')[2])-1
     for doc in self.window.get_documents():
       if 'file://'+doc.get_uri_for_display() == file_uri:
         tab = Gedit.Tab.get_from_document(doc)
